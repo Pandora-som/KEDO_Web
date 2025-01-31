05f6837f1cf7bc65634626b52d1711e38afd5c43
Mari <testirovsik0005@mail.ru>
2025-01-31 16:12:57 +0300

    пробный вывод из бд

---
+++ app/Http/Controllers/DocumentFromController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\DocumentFrom;
+
+class DocumentFromController extends Controller
+{
+    public function index(){
+        $w = DocumentFrom::all();
+        return view("mainsreen", compact("w"));
+    }
+}
--- database/migrations/2025_01_31_120720_create_document_froms_table.php
+++ database/migrations/2025_01_31_120720_create_document_froms_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('document_froms', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("organisation_name");
+            $table->softDeletes();
         });
     }
 
