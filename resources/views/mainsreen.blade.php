c1963c5e009eca4de04f9430680ad6bc982ebcd4
Mari <testirovsik0005@mail.ru>
2025-02-01 21:40:53 +0300

    начала вёрстку, шапка

--- resources/views/mainsreen.blade.php
+++ resources/views/mainsreen.blade.php
@@ -4,10 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset(path: 'css/main.css') }}">
 </head>
 <body>
-    @foreach ($w as $i)
+    {{--@foreach ($w as $i)
         {{$i->organisation_name}}
-    @endforeach
+    @endforeach--}}
+
+    <div class="header_line">
+        <nav class="nav">
+            <a href="#" class="link">Исходящие документы</a>
+            <a href="#" class="link">Входящие документы</a>
+        </nav>
+        <div class="circle"></div>
+    </div>
+
 </body>
 </html>
